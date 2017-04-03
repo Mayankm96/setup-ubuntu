5bf5c89b4f7f351001b3fddacd7a65ce53b776d3
Mayank Mittal <mayankm.iitk@gmail.com>
2017-04-04 01:33:19 +0530

    format script

--- setup_clean.sh
+++ setup_clean.sh
@@ -7,38 +7,49 @@ ROS_DISTRO = $'kinetic'
 git_username = $'Mayankm96'
 git_email_id = $'mayankmittal155@gmail.com'
 
+echo $'\n'
 STR=$'WARNING: This script is meant for Ubuntu 16.04.2LTS. In case you are using any other OS, the script would not work properly and there might be dependancies issues.'
-echo "$STR"
+echo "$STR" $'\n'
 
 sudo apt-get update
 
+# Blank Line
+echo $'\n'
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo $'\n'
+
 # setup oh-my-zsh
-echo "Installing oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)"
+echo "Installing oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)" $'\n'
 sudo apt-get install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s $(which zsh)
 
 # Blank Line
+echo $'\n'
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo $'\n'
 
 # setup VIM
-echo "Installing vim"
+echo "Installing vim" $'\n'
 sudo apt-get install vim
 
 # Blank Line
+echo $'\n'
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo $'\n'
 
 # setup Sublime Text Editor
-echo "Installing Sublime Text 3"
+echo "Installing Sublime Text 3" $'\n'
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
-
 # Blank Line
+echo $'\n'
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo $'\n'
 
 # setup ROS
-echo "Installing ROS $ROS_DISTRO" 
+echo "Installing ROS $ROS_DISTRO" $'\n'
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
@@ -51,12 +62,13 @@ echo "source /opt/ros/$ROS_DISTRO/setup.zsh" >> ~/.zshrc
 source ~/.zshrc
 sudo apt-get install python-rosinstall
 
-
 # Blank Line
+echo $'\n'
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo $'\n'
 
 # setup arduino
-echo "Installing Arduino and rosserial-arduino" 
+echo "Installing Arduino and rosserial-arduino" $'\n'
 sudo apt-get install arduino
 
 # setup rosserial arduino
@@ -67,18 +79,23 @@ rm -rf ros_lib
 rosrun rosserial_arduino make_libraries.py .
 
 # Blank Line
+echo $'\n'
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo $'\n'
 
 # setup git
+echo "Installing git" $'\n'
 sudo apt-get install git
 git config --global user.name "$git_username"
 git config --global user.email "$git_email_id"
 
 # Blank Line
+echo $'\n'
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo $'\n'
 
 # setup OpenCV 3.0-beta
-echo "Installing OpenCV 3.0.0-beta" 
+echo "Installing OpenCV 3.0.0-beta" $'\n'
 sudo apt-get install build-essential
 sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
@@ -93,19 +110,21 @@ make -j8 # -j8 runs 8 jobs in parallel.
 sudo make install
 
 # Blank Line
+echo $'\n'
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo $'\n'
 
 # setup google chrome
-echo "Installing Google Chrome" 
+echo "Installing Google Chrome" $'\n'
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 echo "In case an error is encountered in above step, run `sudo apt-get -f install`"
 
 # Blank Line
+echo $'\n'
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo $'\n'
 
 echo "Installation of all basic softwares and packages successful!"
 echo "It is recommended that you restart your computer and start afresh."
-
-
