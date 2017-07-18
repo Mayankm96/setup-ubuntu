63b142e7335fabf24a07ebbbc8e15b162ab554a4
Mayankm96 <mayankmittal155@gmail.com>
2017-07-18 18:02:52 +0200

    new setup atom, terminator, opencv 3.2

--- setup_clean.sh
+++ setup_clean.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function blank_line {
+	echo $'\n'
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+	echo $'\n'
+}
+
 # A script made to install all the packages and softwares, frequently required after a clean install of Ubuntu OS. 
 
 echo -e "Enter your github user.name: "
@@ -11,32 +17,29 @@ read git_email_id
 ROS_DISTRO = "kinetic"
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
 echo "Thank you for entering your information!" $'\n'
 read -p "Press enter to continue: "
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
-STR=$'WARNING: This script is meant for Ubuntu 16.04.2LTS. In case you are using any other OS, the script would not work properly and there might be dependancies issues.'
-echo "$STR" $'\n'
+STR=$'WARNING: This script is meant for Ubuntu 16.04.2LTS_64-bit In case you are using any other OS, the script would not work properly and there might be dependancies issues.'
+
+if [[ `lsb_release -rs` != "16.04" ]] # replace 8.04 by the number of release you want
+then
+	echo "$STR" $'\n'
+fi
+
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
 sudo apt-get update
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
 # setup oh-my-zsh
 echo "Installing oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)" $'\n'
@@ -45,48 +48,46 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 chsh -s $(which zsh)
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
 # setup VIM
 echo "Installing vim" $'\n'
 sudo apt-get install vim
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
-# setup Sublime Text Editor
-echo "Installing Sublime Text 3" $'\n'
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+# setup atom-text-editor
+echo "Installing Atom Text Editor" $'\n'
+sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
-sudo apt-get install sublime-text-installer
+sudo apt install atom
+
+# setup Terminator
+echo "Installing Terminator" $'\n'
+sudo add-apt-repository ppa:gnome-terminator
+sudo apt-get update
+sudo apt-get install terminator
+
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
 # setup ROS
-echo "Installing ROS $ROS_DISTRO" $'\n'
+echo "Installing ROS ${ROS_DISTRO}" $'\n'
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install ros-$ROS_DISTRO-desktop-full
+sudo apt-get install ros-${ROS_DISTRO}-desktop-full
 sudo rosdep init
 rosdep update
-echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-echo "source /opt/ros/$ROS_DISTRO/setup.zsh" >> ~/.zshrc
+echo "source /opt/ros/${ROS_DISTRO}/setup.zsh" >> ~/.zshrc
 source ~/.zshrc
 sudo apt-get install python-rosinstall
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
-
+blank_line
 
 # setup Gazebo
 echo "Installing Gazebo" $'\n'
@@ -104,9 +105,7 @@ rm -rf ros_lib
 rosrun rosserial_arduino make_libraries.py .
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
 # setup git
 echo "Installing git" $'\n'
@@ -115,29 +114,21 @@ git config --global user.name "$git_username"
 git config --global user.email "$git_email_id"
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
-# setup OpenCV 3.0-beta
-echo "Installing OpenCV 3.0.0-beta" $'\n'
-sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-cd /tmp
-git clone https://github.com/Itseez/opencv.git
-cmake /tmp/opencv
-mkdir -p /tmp/opencv/release
-cd /tmp/opencv/release
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
-make -j8 # -j8 runs 8 jobs in parallel.
-         # Change 8 to number of hardware threads available.
-sudo make install
+# setup OpenCV 3.2.0
+echo "Installing OpenCV 3.2.0" $'\n'
+./install_openCV.sh
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
+
+# setup OpenCV 3.2.0
+echo "Installing Python-PIP" $'\n'
+sudo apt-get install python-pip
+
+# Blank Line
+blank_line
 
 # setup google chrome
 echo "Installing Google Chrome" $'\n'
@@ -147,9 +138,7 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 echo "In case an error is encountered in above step, run `sudo apt-get -f install`"
 
 # Blank Line
-echo $'\n'
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo $'\n'
+blank_line
 
 echo "Installation of all basic softwares and packages successful!"
 echo "It is recommended that you restart your computer and start afresh."
